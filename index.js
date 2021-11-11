99fa4f9d61f148c69c2cd38441c1e47a0467e88b
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-11 11:22:43 -0500

    add
    javaScript

---
+++ index.js
@@ -0,0 +1,40 @@
+let number = 8;
+//global ozgaruvchi
+function myFunction(text){
+    console.log(text);
+   let numbers = 10;
+   console.log(numbers);
+   //ichki ozgaruvchi
+}
+myFunction("hello text");
+console.log(number);
+
+
+function calc(a, b){
+return a+b;
+
+}
+console.log(calc(10, 12));
+
+
+
+function calcFunction(){
+    let numbertwo = 8;
+    return numbertwo;
+
+}
+
+const num = calcFunction();
+console.log(num);
+
+
+
+const funcexp = function (){
+    console.log(19);
+}
+funcexp();
+
+
+const arryfunct = (f, c) => f + c;
+const puls = arryfunct(12 ,13);
+console.log(plus);
